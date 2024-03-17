8e279bfe94d6634d5c99565deb71ce61761becc3
smilivoje <stalke8828@gmail.com>
2024-03-17 14:26:54 +1000

    staking external

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -79,6 +79,7 @@ function TopBar({ colorTheme, toggleTheme, handleDrawerToggle }: TopBarProps) {
               }
               <NavItem to="/nodes" label={`Nodes`} />
               <NavItem to="/lending" label={`Lending`} />
+              <NavItem href="https://staking.nodesynapse.app/" label={`Staking`} />
               {/* <NavItem to="/leaderboard" label={`Leaderboard`} /> */}
             </>
           )}
